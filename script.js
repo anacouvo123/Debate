0351c69b3f32533caff63fce4f2d0378a75d566e
anacouvo123 <130079384+anacouvo123@users.noreply.github.com>
2024-08-02 14:35:27 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -19,56 +19,46 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Para promover a sustentabilidade ambiental qual das seguintes ações é mais eficaz?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Gerenciar resíduos de forma eficiente para reduzir a poluição e conservar recursos.",
+                afirmacao: "O gerenciamento de resíduos minimiza o desperdício, reduz a poluição e preserva recursos."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Investir em energia eólica para gerar eletricidade e reduzir a depêndencia de combustivel fossil.",
+                afirmacao: "A energia eólica é uma fonte renovavel e limpa que ajuda a reduzir a emissão de gases."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Como a implementação de energia eólica pode impactar o gerenciamento de resíduos e a conservação de água em uma região?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "A energia eólica, ao substituir fontes de energia baseadas em combustíveis fósseis, reduz a produção de resíduos tóxicos e a poluição da água.",
+                afirmacao: "A energia eólica melhora o gerenciamento de resíduos e conserva a água, promovendo um ambiente mais limpo."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "A produção e a manutenção de turbinas eólicas podem gerar resíduos industriais específicos e, em algumas fases, demandar uso intenssivo de água.",
+                afirmacao: "É crucial adotar práticas de reciclagem e gestão de resíduos e minimizar ouso de água para maximizar a sustentabilidade da energia eólica."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "De que maneira a conservação da água pode ser afetada pela expansão de parques de energia eólica?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "A energia eólica requer pouca água para operação, ao contrário de outras energias.",
+                afirmacao: "A expansão de parques eólicos contribui para a conservação de recursos hídricos, levando mais água para outros usos essenciais na comunidade."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "A construção de parques eólicos pode afetar o uso do solo, importando os recursos hídricos.",
+                afirmacao: "Apesar da operação de parques eólicos conservar água é crucial planejar e emitir impactos durante a construção para protejer os recursos hídricos."
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
-        alternativas: [
-            {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
-            },
-            {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
-            }
-        ]
+        
     },
 ];
 
